fe33c9d5860d6bfcd9be94a531faf315ca1e92ec
shtigran <shakhbekyantigran@gmail.com>
2017-01-19 22:12:05 +0400

    Tree Directory Creator

---
+++ ClassLibrary1/ClassLibrary1/CreateTree.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TreeDirectory
+{
+    public static class CreateTree
+    {
+        static string pathFile;
+
+        public static void Scan(this string path, string punct)
+        {
+            DirectoryInfo direct = new DirectoryInfo(path);
+            DirectoryInfo[] listOfDir = direct.GetDirectories(); 
+
+            foreach (DirectoryInfo dd in listOfDir) 
+            {
+
+                if (dd.Attributes == FileAttributes.Directory) 
+                {              
+                    if (dd == listOfDir.Last())
+                    {
+                        Console.WriteLine(punct + "-" + dd);
+                        try
+                        {
+                            pathFile = path + "\\" + dd;
+                            string[] dirs = Directory.GetFiles(pathFile);
+                            foreach (string dir in dirs)
+                            {
+                                Console.WriteLine(punct + "  -" + dir);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("The process failed: {0}", e.ToString());
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine(punct + "-" + dd);
+                        try
+                        {
+                            pathFile = path + "\\" + dd;
+                            string[] dirs = Directory.GetFiles(pathFile);
+                            foreach (string dir in dirs)
+                            {
+                                Console.WriteLine(punct + "  -" + dir);
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Console.WriteLine("The process failed: {0}", e.ToString());
+                        }
+                    }
+
+                    try 
+                    {
+                        DirectoryInfo dir = new DirectoryInfo(path + "\\" + dd);
+                        DirectoryInfo[] listOfDirs = dir.GetDirectories();
+                        if (listOfDirs.Length > 0)
+                        {
+
+                            if (dd != listOfDir.Last())
+                            {
+                                Scan(path + "\\" + dd, punct + "  ");
+                            }
+                            else
+                            {
+                                Scan(path + "\\" + dd, punct + "  ");
+                            }
+                        }
+                    }
+                    catch { }               
+                }
+            }
+        }
+    }
+}
---
+++ ClassLibrary1/Program/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using TreeDirectory;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("       ______Directory Tree Creator ______\n");
+            Console.WriteLine("Please enter the location path of the folder,");
+            Console.WriteLine("which Directory Tree Yoy want to create.");
+            Console.Write("Location path format: C:\\Users\\User\\Desktop\\translate.txt): ");
+
+            string path = Console.ReadLine();
+            Console.WriteLine("\nThe directory tree of " + path + " is: \n");
+            path.Scan("");
+            string[] fi = Directory.GetFiles(path);
+            foreach (string dir in fi)
+            {
+                Console.WriteLine("-" + dir);
+            }
+            Console.ReadKey();
+        }
+    }
+}
